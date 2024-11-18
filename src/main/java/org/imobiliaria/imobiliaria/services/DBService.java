accbc6357c23758b158bb836a07b0a31231be930
Lechinovski <vteixeiraj@outlook.com>
2024-11-18 08:40:33 -0300

    att

--- src/main/java/org/imobiliaria/imobiliaria/services/DBService.java
+++ src/main/java/org/imobiliaria/imobiliaria/services/DBService.java
@@ -24,10 +24,5 @@ public class DBService {
     @Bean
     public void instanciaBaseDeDados() throws ParseException {
 
-        SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-        VendedorModel vendedorModel1 = new VendedorModel("João", "12345678901", "Vendedor", formato.parse("09/09/2024"), false);
-        VendedorModel vendedorModel2 = new VendedorModel("Bruno", "98765432100", "Corretor", formato.parse("16/09/2024"), true);
-        VendedorModel vendedorModel3 = new VendedorModel("Teste", "11122233344", "TI", formato.parse("23/09/2024"), true);
-        vendedorRepository.saveAll(Arrays.asList(vendedorModel1, vendedorModel2, vendedorModel3));
     }
 }
